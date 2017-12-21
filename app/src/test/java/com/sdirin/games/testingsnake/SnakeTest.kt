155f13b40e6fefd3c7402419801a4e1599c2fd7d
Sergey Dirin <sdirin@gmail.com>
2017-12-22 01:47:49 +0400

    Initial commit

---
+++ app/src/test/java/com/sdirin/games/testingsnake/SnakeTest.kt
@@ -0,0 +1,15 @@
+package com.sdirin.games.testingsnake
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+* Created by SDirin on 22-Dec-17.
+*/
+
+class SnakeTest {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
